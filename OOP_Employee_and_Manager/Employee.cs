544cc79c2a0261bbfec13960fda4832274940b27
wxrstvrsn <prestige.2014.xx@gmail.com>
2025-04-10 20:59:53 +0300

    Добавьте файлы проекта.

---
+++ OOP_Employee_and_Manager/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Employee_and_Manager
+{
+    internal class Employee
+    {
+        private string _name;
+        private  float _salary;
+        private DateTime _dateOfDirth;
+        private DateTime _employDate;
+    
+        //C-tors
+
+        /// <summary>
+        /// Возвращает кол-во отработанных дней 
+        /// </summary>
+        private int GetExperience()
+        {
+
+            return 0;
+        }
+        /// <summary>
+        /// Увеличивает зарплату на процент из param.
+        /// <param name="percent"> Процент увеличения 0 - 100 </param>
+        /// </summary>
+        private void IncreaseSalary(float percent)
+        {
+            _salary += _salary * (percent / 100f);
+        }
+    }
+
+}
---
+++ OOP_Employee_and_Manager/Form1.cs
@@ -0,0 +1,10 @@
+namespace OOP_Employee_and_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
